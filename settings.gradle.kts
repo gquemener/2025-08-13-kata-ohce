5bdf7dc1d5e9395c3e8ae39d9c880aaf912cd745
Gildas Quéméner <gildas.quemener-ext@nickel.eu>
2025-08-14 12:21:57 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "2025-08-14-ohce"
---
+++ src/test/java/org/example/ExampleTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ExampleTest {
+  @Test
+  void example() {
+    assertThat(true).isEqualTo(false);
+  }
+}
