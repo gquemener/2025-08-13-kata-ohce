f9ca909764c84ce79f9ddacb5a61b250c2c0bf0b
Gildas Quéméner <gildas.quemener-ext@nickel.eu>
2025-08-14 12:26:03 +0200

    Add instructions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,21 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+val junitVersion = "5.13.4"
+val mockitoVersion = "5.19.0"
+val assertJVersion = "3.27.4"
+
 repositories {
     mavenCentral()
 }
-
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:${junitVersion}"))
+    testImplementation(platform("org.mockito:mockito-bom:${mockitoVersion}"))
+    testImplementation(platform("org.assertj:assertj-bom:${assertJVersion}"))
+
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.assertj:assertj-core:3.27.3")
+    testImplementation("org.mockito:mockito-core")
+    testImplementation("org.assertj:assertj-core")
 }
 
 tasks.test {
