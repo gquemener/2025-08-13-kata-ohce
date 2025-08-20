af9830c36ccac2bdd7ef0b23b9c4b23783f7bf11
Gildas Quéméner <gildas.quemener-ext@nickel.eu>
2025-08-20 17:16:47 +0200

    start translating example

--- src/test/java/org/example/OhceFeature.java
+++ src/test/java/org/example/OhceFeature.java
@@ -1,25 +1,45 @@
 package org.example;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class OhceFeature {
+
+  @Mock private Console console;
+
   @Test
   void example() {
+    var app = new Ohce(console);
+    app.start("Pedro");
+    verify(console).printLn("> ¡Buenos días Pedro!");
+
     /*
-$ ohce Pedro
-> ¡Buenos días Pedro!
-$ hola
-> aloh
-$ oto
-> oto
-> ¡Bonita palabra!
-$ stop
-> pots
-$ Stop!
-> Adios Pedro
-     */
+    $ hola
+    > aloh
+    $ oto
+    > oto
+    > ¡Bonita palabra!
+    $ stop
+    > pots
+    $ Stop!
+    > Adios Pedro
+         */
     assertThat(true).isEqualTo(false);
+  }
+
+  private interface Console {
+    void printLn(String s);
+  }
+
+  private class Ohce {
+    public Ohce(Console console) {}
+
+    public void start(String pedro) {}
   }
 }
