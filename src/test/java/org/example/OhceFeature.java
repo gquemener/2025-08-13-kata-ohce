6f1231f4137c41cf1ea1ab7fe96ad1c9dd1f59fc
David Franck <david.franck-ext@nickel.eu>
2025-08-20 17:04:53 +0200

    hop

--- src/test/java/org/example/OhceFeature.java
+++ src/test/java/org/example/OhceFeature.java
@@ -4,9 +4,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-class ExampleTest {
+class OhceFeature {
   @Test
   void example() {
+    /*
+$ ohce Pedro
+> ¡Buenos días Pedro!
+$ hola
+> aloh
+$ oto
+> oto
+> ¡Bonita palabra!
+$ stop
+> pots
+$ Stop!
+> Adios Pedro
+     */
     assertThat(true).isEqualTo(false);
   }
 }
